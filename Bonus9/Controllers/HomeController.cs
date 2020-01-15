1a94a4a9b63e83afade7bad7a25327e5a76a7555
Jason <jasonrizzo02@aol.com>
2020-01-14 21:16:12 -0500

    first try

---
+++ Bonus9/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bonus9.Models;
+
+namespace Bonus9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        //ADDING NEW ACTION CAN LOAD A NEW VIEW
+        public IActionResult About()
+        {
+            //RETURN VIEW
+            return View();
+        }
+
+        public IActionResult Portfolio() 
+        {
+            return Portfolio();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
